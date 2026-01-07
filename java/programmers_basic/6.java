688618f346c3cbad01895e8186122ed6b61b8d50
Nahyun <65719936+nhkim06@users.noreply.github.com>
2026-01-07 16:30:28 +0900

    프로그래머스 6

---
+++ java/programmers_basic/6.java
@@ -0,0 +1,26 @@
+/*
+    link : https://school.programmers.co.kr/learn/courses/30/lessons/120913
+ */
+class Solution {
+    public String[] solution(String my_str, int n) {
+        int idx = 0;
+        int arrIdx = my_str.length()/n;
+        if ( my_str.length()%n != 0 ) arrIdx += 1;
+        String[] answer = new String[arrIdx];
+        
+        for (int i=0; i<arrIdx-1; i++){
+            answer[i] = my_str.substring(n*i, n*i+n);
+        }
+        answer[arrIdx-1] = my_str.substring(n*(arrIdx-1));
+        
+        return answer;
+        
+    }
+}
+
+/*
+    문제 해결 : 
+        - my_str/n으로 answer의 element 개수 구하기
+        - 각각의 index에 substring으로 n만큼 잘라 넣기
+        - my_str/n의 나머지 에 해당하는 값들은 sunstring(n)으로 처리
+*/
