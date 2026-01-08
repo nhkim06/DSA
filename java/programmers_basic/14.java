e059a2e373b13845ba5651cb1dc0f85c6a88d569
Nahyun <65719936+nhkim06@users.noreply.github.com>
2026-01-08 23:25:27 +0900

    프로그래머스 13, 14

--- java/programmers_basic/14.java
+++ java/programmers_basic/14.java
@@ -1,23 +1,23 @@
 /*
     link : https://school.programmers.co.kr/learn/courses/30/lessons/120905
  */
+import java.util.ArrayList;
+
 class Solution {
-    public int solution(int n) {
-        String num = String.valueOf(n);
-        int sum = 0;
-        
-        for (int i=0; i<num.length(); i++) {
-            sum += num.charAt(i) - '0';
+    public int[] solution(int n, int[] numlist) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int num : numlist) {
+            if (num % n == 0) {
+                list.add(num);
+            }
         }
-        return sum;
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
     }
 }
-
-/*
-    문제 해결
-        1. String으로 바꿔서 각 자리 수를 가져오기.
-            - 각 자리수를 다시 int로 바꿔 sum에 더하기
-            - Integer.parseInt(String.valueOf(num.charAt(i))) 불필요한 형 변환이 있음
-        2. char의 특성 활용하기
-            - num.charAt(i) - '0’
-*/
