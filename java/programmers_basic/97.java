9f3faa8d7364947e99e61f6d78ba38cce9c364ee
Nahyun <65719936+nhkim06@users.noreply.github.com>
2026-01-18 14:19:48 +0700

    Update Numbers (최신순)

---
+++ java/programmers_basic/97.java
@@ -0,0 +1,9 @@
+/*
+    link : https://school.programmers.co.kr/learn/courses/30/lessons/120803
+*/
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+}
