046613968c17757648f1692731eeb98d91e1c780
Nahyun <65719936+nhkim06@users.noreply.github.com>
2026-01-20 18:18:25 +0700

    프로그래머스 27번

--- java/programmers_basic/27.java
+++ java/programmers_basic/27.java
@@ -1,3 +1,17 @@
 /*
     link : https://school.programmers.co.kr/learn/courses/30/lessons/120890
 */
+class Solution {
+    public int solution(int[] array, int n) {
+        int ans = array[0];
+        for (int i=1; i<array.length; i++){
+            int difAns = Math.abs(n-ans);
+            int difCur = Math.abs(n-array[i]);
+            
+            if( difCur < difAns ) ans = array[i];
+            else if ( difCur == difAns && array[i] < ans ) ans = array[i];
+        }
+        
+        return ans;
+    }
+}
