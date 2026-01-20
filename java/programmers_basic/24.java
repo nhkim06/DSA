0e93cb57ca4c62eb9d535090e4edca32f20afcf1
Nahyun <65719936+nhkim06@users.noreply.github.com>
2026-01-20 17:16:33 +0700

    프로그래머스 24번

--- java/programmers_basic/24.java
+++ java/programmers_basic/24.java
@@ -1,3 +1,21 @@
 /*
     link : https://school.programmers.co.kr/learn/courses/30/lessons/120893
 */
+class Solution {
+    public String solution(String my_string) {
+        char[] array = my_string.toCharArray();
+        for (int i=0; i<my_string.length(); i++){
+            if (Character.isUpperCase(array[i])){
+                array[i] = Character.toLowerCase(array[i]);
+            }else{
+                array[i] = Character.toUpperCase(array[i]);
+            }
+        }
+        return new String(array);
+    }
+}
+
+/*
+    문제 해결
+    - 대소문자인지 구분 후 소문자이면 소문자, 대문자이면 대문자로 변환
+*/
