4ed7ee95045e2c2d3c1cb9b025641d89d5f3a13b
Nahyun <65719936+nhkim06@users.noreply.github.com>
2026-01-20 17:04:53 +0700

    프로그래머스 23번

--- java/programmers_basic/23.java
+++ java/programmers_basic/23.java
@@ -1,3 +1,13 @@
 /*
     link : https://school.programmers.co.kr/learn/courses/30/lessons/120894
 */
+class Solution {
+    public long solution(String numbers) {
+        String[] nums = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};        
+        for (int i=0; i<10; i++){
+            numbers = numbers.replace(nums[i], String.valueOf(i));
+        }
+        
+        return Long.parseLong(numbers);
+    }
+}
