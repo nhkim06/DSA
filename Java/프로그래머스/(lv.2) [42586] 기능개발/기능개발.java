5434299988dd1d13716a883504600a9a6dddcd82
Nahyun <65719936+nhkim06@users.noreply.github.com>
2026-04-21 16:37:09 +0900

    [level 2] Title: 기능개발, Time: 3.51 ms, Memory: 85 MB -BaekjoonHub

---
+++ Java/프로그래머스/(lv.2) [42586] 기능개발/기능개발.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int days = (int) Math.ceil((100.0-progresses[0])/speeds[0]);
+        List<Integer> answer = new ArrayList<>();
+        
+        int count = 0;
+        for (int i=0; i<progresses.length; i++){
+            int curProgress = progresses[i] + days * speeds[i];
+            if( curProgress >= 100 ){
+                count++;
+            }else{
+                answer.add(count);
+                count = 1;
+                days += (int) Math.ceil((100.0-curProgress)/speeds[i]);
+            }
+        }
+        answer.add(count);
+
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}
+
+/*
+문제 해석
+- in  : 작업 순서 + 진행도, 작업 속도
+- out : 각 배포마다 몇 개의 기능이 배포되는지
+조건 
+- 앞에거가 완성되어야 뒤에거도 배포 가능. 
+
+문제 해결
+1. days로 며칠이 지났는지 카운트하기. 
+    1) 일단 첫 번째꺼 배포할 때까지 day count. 
+    2) 뒤에거 확인
+2. 작업 소요일 수 적혀있는 queue 만들고 배포일 확인 
+
+*/
