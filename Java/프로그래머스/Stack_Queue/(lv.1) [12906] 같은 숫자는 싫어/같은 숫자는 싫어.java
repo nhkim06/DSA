3168da85a2946853edb62a1470bdba6431f6fe6b
nhkim06 <nhkim7788@gmail.com>
2026-04-21 19:37:54 +0900

    구조 변경

--- Java/프로그래머스/Stack_Queue/(lv.1) [12906] 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ Java/프로그래머스/Stack_Queue/(lv.1) [12906] 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -23,7 +23,7 @@ public class Solution {
     1) peek 했을 때 만약 값이 현재 인덱스의 배열 값과 같다면 넘어가고
     2) 만약 값이 다르다면 push 하기. 
     3) stack을 int[]로  변환
-2. 더 빠른 ArrayList만 사용하기
+2. ArrayList만 사용하기
     1) ArrayList에 배열의 첫 요소 넣고 last를 배열의 첫 요소로 설정하기
     2) 순차적으로 배열의 값들을 last와 비교하며
         - 만약 현재 배열값 == last라면 넘어가고,
