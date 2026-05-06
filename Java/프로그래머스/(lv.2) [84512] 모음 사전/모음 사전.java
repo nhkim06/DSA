9c69712684cb4ad08c37839339569ef0b292ad07
Nahyun <65719936+nhkim06@users.noreply.github.com>
2026-05-06 11:32:31 +0900

    [level 2] Title: 모음 사전, Time: 0.04 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/(lv.2) [84512] 모음 사전/모음 사전.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(String word) {
+        int[] weight = new int[]{ 781, 156, 31, 6, 1 };
+        String vowels = "AEIOU";
+        int count = 0;
+
+        for(int i=0; i<word.length(); i++){
+            count += vowels.indexOf(word.charAt(i)) * weight[i] + 1;
+        }
+        
+        return count;
+    }
+}
+
+/*
+문제 해결
+1. 미리 모든 가능한 경우를 hashmap에 저장해놓고 바로 조회 : 
+    - 결국 전체를 순회한 후에 찾는거니 1회성 메소드엔 비효율적.
+2. 모든 경우 탐색하며 count하다가, 만약 word와 일치하면 count return 
+    - DFS vs BFS : '모든 경우' 문제는 DFS
+3. charAt() 사용해서 순서 계산? 5진수 느낌이 나는게 뭔가 계산할 수 있을 것 같음
+    - 예상
+        - 5^4 + 5^3 + 5^2 + 5^1 + 5^0 = 781
+        - 5^3 + 5^2 + 5^1 + 5^0 = 156
+        - 5^2 + 5^1 + 5^0 = 31
+        - 5^1 + 5^0 = 6
+        - 5^0 = 1
+        여기에 글자 인덱스 k를 곱하여 계산하면 되지 않을까
+
+*/
