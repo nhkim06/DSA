2c9c49cce9f02e4e4bfcb7563ef4116767f85eda
nhkim06 <nhkim7788@gmail.com>
2026-05-12 11:23:26 +0900

    code : hash

---
+++ Java/LeetCode/twoSum/1_완전탐색.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i=0; i<nums.length-1; i++){
+            for (int j=i+1; j<nums.length; j++){
+                if ( nums[i] + nums[j] == target ) return new int[]{i, j};
+            }
+        }
+        return new int[2];
+    }
+}
+
+/*
+문제 해석
+- indices of two nums that add up to target
+
+
+문제 해결
+- 완전탐색 : target-cur 값이 배열에 있는지 확인하기
+- hash 사용하기
+    1. hashmap에 각 값 넣기 O(n) (key=nums요소값, value=index)
+    2. nums의 값을 순차적으로 탐색. hashmap에 target-cur가 있는지 확인
+    3. index반환
+
+
+ */
---
+++ Java/LeetCode/twoSum/README.md
@@ -0,0 +1,5 @@
+[문제 링크](https://leetcode.com/problems/two-sum/submissions/2000944418/)
+
+```
+
+```
